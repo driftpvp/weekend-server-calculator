2b720849466e5cd1d4cb874a1c24d74d0a7f2ca1
Lucas Martin <driftpvp@gmail.com>
2023-08-01 16:05:33 -0500

    adding GET requests

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,15 @@
 console.log('hello calculators');
+
+function getCalculations(){
+    console.log('in getCalculations');
+    //axios call to server to get calculations
+    axios.get("/calculations/").then((response) => {
+        let calculations = response.data;
+        console.log(calculations);
+
+        updateDom(calculations);
+    })
+    .catch((error) => {
+        console.log("error in GET", error);
+    });
+}// end getCalculations
--- server/routes/calc.router.js
+++ server/routes/calc.router.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const calcRouter = express.Router();
 
+let calc = [
+    {
+        integerOne: 1,
+        integerTwo: 2
+    }
+];
+
+calcRouter.get('/', (req, res) => {
+    res.send(calc);
+    console.log(calc);
+})
+
 
 module.exports = calcRouter;
