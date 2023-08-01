2f86289f16cc53138b149581cf8e1424e690c3ce
Lucas Martin <driftpvp@gmail.com>
2023-08-01 15:45:33 -0500

    set up routes

---
+++ server/routes/calc.router.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const calcRouter = express.Router();
+
+
+module.exports = calcRouter;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5001;
+const calcRouter = require('./routes/calc.router')
+
+app.use(express.json());
 app.use(express.static('sever/public'));
+
+
 app.listen(port, () => {
     console.log('listening on port', port);
 })
