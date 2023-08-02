80605eb0b990762410bbf1b945873fab260e3a5d
Lucas Martin <driftpvp@gmail.com>
2023-08-02 13:43:46 -0500

    two get and two post

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -13,3 +13,33 @@ function getCalculations(){
         console.log("error in GET", error);
     });
 }// end getCalculations
+
+function submitCalculations(event){
+    console.log('in submitCalculations');
+    event.preventDefault();
+    let integerOne = Numberdocument.querySelector('#firstNumber').value
+    let integerTwo = Numberdocument.querySelector('#secondNumber').value
+
+    function mathsNumbers(event){
+        console.log('operator', event.target.innerHTML);
+        let calc = [
+            {
+                integerOne: '',
+                operator: '',
+                integerTwo: ''
+            }
+        ];
+
+        axios.post("/calculations", calc).then((response) => {
+            console.log('back from POST:', response);
+            document.querySelector('#firstNumber').value = ''
+            document.querySelector('#secondNumber').value = ''
+            getCalculations();
+        })
+        .catch((error) => {
+            console.log('error in POST', error);
+        })
+
+        document.querySelector('#clearButton').reset();
+    }
+}
--- server/routes/calc.router.js
+++ server/routes/calc.router.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const calcRouter = express.Router();
 
-let calc = [
-    {
-        integerOne: 1,
-        integerTwo: 2
-    }
-];
+
 
 calcRouter.get('/', (req, res) => {
     res.send(calc);
     console.log(calc);
+})
+
+calcRouter.post('/', (req, res) => {
+    console.log('get a POST request.', req.body);
+
+    let calculations = req.body
+    calc.push(calculations)
+    res.sendStatus(201);
 })
 
 
